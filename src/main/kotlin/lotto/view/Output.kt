8416a6ed6a00fff40c140c116e6a74e0a6de2b8e
choi-day <choisschoco@gmail.com>
2025-02-18 02:51:31 +0900

    fix : 구매한 로또 번호 출력 수정
    Co-authored-by: Hyemin <143622050+hyeminililo@users.noreply.github.com>

--- src/main/kotlin/lotto/view/Output.kt
+++ src/main/kotlin/lotto/view/Output.kt
@@ -1,12 +1,12 @@
 package lotto.view
 
 import lotto.model.Lotto
-import lotto.view.Input.getPurchase
 
 object Output {
     fun printPurchase() = println("구입금액을 입력해 주세요.")
 
     fun printTicketNumber( purchas : Int) = println("${purchas}개를 구매했습니다.")
 
-    fun printTickets(tickets: ArrayList<Lotto>) =  println(tickets.joinToString("\n"))
+    fun printTickets(tickets: ArrayList<Lotto>) =  tickets.forEach { lotto -> println(lotto.getNumbers) }
+//    fun printTickets(tickets: ArrayList<Lotto>) =  println(tickets.joinToString("\n"))
 }
