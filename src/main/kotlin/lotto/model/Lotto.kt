8c25ee8fdc724a257d999a5dac6e3b95c2705fbb
choi-day <choisschoco@gmail.com>
2025-02-18 01:50:40 +0900

    feat: TicketsController작성
    Co-authored-by: Hy성emin <143622050+hyeminililo@users.noreply.github.com>

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -2,19 +2,13 @@ package lotto.model
 
 import camp.nextstep.edu.missionutils.Randoms
 
-private const val MAX_RANGE = 45
-private const val MIN_RANGE = 1
-private const val LOTTO_NUMBER_COUNT = 6
-
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
-        numbers = Randoms.pickUniqueNumbersInRange(MIN_RANGE, MAX_RANGE, LOTTO_NUMBER_COUNT).sorted()
-
     }
 
     fun getRandoms():List<Int>{
-        numbers = Randoms.pickUniqueNumbersInRange(MIN_RANGE, MAX_RANGE, LOTTO_NUMBER_COUNT).sorted()
+//        numbers = Randoms.pickUniqueNumbersInRange(MIN_RANGE, MAX_RANGE, LOTTO_NUMBER_COUNT).sorted()
         return numbers
     }
 }
